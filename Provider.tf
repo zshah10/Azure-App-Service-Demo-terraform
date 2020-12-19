fdf08d600bf16b5b45cb88fd7ad4c20cae188c7c
zshah10 <zpert.2000@hotmail.co.uk>
2020-12-19 09:27:04 +0000

    Create Provider.tf

---
+++ Provider.tf
@@ -0,0 +1,11 @@
+# Define Terraform provider
+terraform {
+  required_version = ">= 0.12"
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  environment = "public"
+  version     = ">= 2.15.0"
+  features {}
+}
